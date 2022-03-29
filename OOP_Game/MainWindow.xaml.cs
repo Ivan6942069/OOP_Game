7d74160cdc2879a84e3b9ed8d3180d09a46ccc1e
sorban.ivan <sorban.ivan@D20-06>
2022-03-29 11:34:13 +0200

    Add project files.

---
+++ OOP_Game/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        charakter charakter1;
+        charakter charakter2;
+        public MainWindow()
+        {
+            InitializeComponent();
+            charakter1 = new charakter("Pert");
+            charakter2 = new charakter("Pavel");
+            Txt1.Text = charakter1.ToString();
+            Txt2.Text = charakter2.ToString();
+        }
+
+        private void Attack_Click(object sender, RoutedEventArgs e)
+        {
+            charakter2.GetHit(charakter1.Damage);
+            Txt2.Text = charakter2.ToString();
+        }
+
+        private void Heal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OOP_Game/charakter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Game
+{
+    class charakter
+    {
+        private int shield;
+        private int health;
+        private int damage;
+
+        public string Name { get; }
+        public int Health { get => health; }
+        public int Shield { get => shield; }
+        public int Damage { get => damage; }
+
+
+        public charakter(string name)
+        {
+            Name = name;
+            health = 100;
+            shield = 50;
+            damage = 20;
+
+        }
+        public void GetHit(int damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                health += shield;
+                shield = 0;
+            }
+        }
+        public void Heal()
+        {
+            health += 15;
+        }
+        public override string ToString()
+        {
+            string text = "Name: " + Name + "\n";
+            text += "Shield: " + Shield + "\n";
+            text += "Health: " + Health + "\n";
+            text += "Damage: " + Damage + "\n";
+            return text;
+        }
+    }
+}
